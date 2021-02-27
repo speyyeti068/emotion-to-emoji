d74fe58abde27aa4ac650097a28e390adc5bb363
speyyeti068 <75049648+speyyeti068@users.noreply.github.com>
2021-02-26 18:39:34 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -12,19 +12,19 @@ camera = document.getElementById("camera");
 
 Webcam.attach("#camera");
 
-function captureImage(){
-    Webcam.snap(function(dataURI){
+function captureImage() {
+    Webcam.snap(function (dataURI) {
         document.getElementById("result").innerHTML = "<img id = 'imageResult' src = '" + dataURI + "'>";
     })
 }
 
 console.log("ML5 Version = " + ml5.version);
 
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/wf-GisCLL/model.json", function(){
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/wf-GisCLL/model.json", function () {
     console.log("Model Loaded")
 })
 
-function speak(){
+function speak() {
     var synth = window.speechSynthesis;
 
     var speechData1 = "The First Prediction Is: " + predict1;
@@ -35,6 +35,40 @@ function speak(){
     synth.speak(speechUtterance);
 }
 
-function predictEmotion(){
-    speak();
+function check() {
+    var webcam = document.getElementById("imageResult");
+
+    classifier.classify(webcam, gotResult);
+}
+
+function gotResult(error, result) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(result);
+
+        document.getElementById("emotionNameResult1").innerHTML = result[0].label;
+        document.getElementById("emotionNameResult2").innerHTML = result[1].label;
+
+        if (result[0].label == "Happy") {
+            document.getElementById("emojiResult1").innerHTML = "&#128522";
+        } else if (result[0].label == "Shocked") {
+            document.getElementById("emojiResult1").innerHTML = "&#x1f632";
+        } else {
+            document.getElementById("emojiResult1").innerHTML = "&#128548";
+        }
+
+        if (result[1].label == "Happy") {
+            document.getElementById("emojiResult2").innerHTML = "&#128522";
+        } else if (result[1].label == "Shocked") {
+            document.getElementById("emojiResult2").innerHTML = "&#x1f632";
+        } else {
+            document.getElementById("emojiResult2").innerHTML = "&#128548";
+        }
+
+        predict1 = result[0].label
+        predict2 = result[1].label;
+
+        speak();
+    }
 }
